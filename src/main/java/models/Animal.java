9d2f52b5563f8a0f05ba932acd47934a44e166aa
George Okumu <gokumu12@gmail.com>
2020-09-20 16:27:28 +0300

    Overrode abstract methods in DatabaseManagement interface

--- src/main/java/models/Animal.java
+++ src/main/java/models/Animal.java
@@ -1,8 +1,12 @@
 package models;
 
+import org.sql2o.Connection;
+
 public abstract class Animal {
     public String animalName;
     public int animalId;
+    public String type;
+    public int id;
 
     public String getAnimalName() {
         return animalName;
@@ -10,6 +14,39 @@ public abstract class Animal {
 
     public int getAnimalId() {
         return animalId;
+    }
+
+
+    public void save() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animals (animalName, animalId, type) VALUES (:animalName, :animalId, :type)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("animalName", this.animalName)
+                    .addParameter("personId", this.animalId)
+                    .addParameter("type", this.type)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+    @Override
+    public boolean equals(Object otherAnimal){
+        if (!(otherAnimal instanceof Animal)) {
+            return false;
+        } else {
+            Animal newAnimal = (Animal) otherAnimal;
+            return this.getAnimalName().equals(newAnimal.getAnimalName()) &&
+                    this.getAnimalId() == newAnimal.getAnimalId();
+        }
+    }
+
+    public void delete() {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "DELETE FROM animals WHERE id = :id;";
+            con.createQuery(sql)
+                    .addParameter("id", this.id)
+                    .executeUpdate();
+        }
     }
 
 }
