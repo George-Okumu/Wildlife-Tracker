045bdccd40316147cd178f9279145f5d323ede49
George Okumu <gokumu12@gmail.com>
2020-09-21 08:57:58 +0300

    Run tests to test the instances

--- src/main/java/models/NonEndangeredAnimal.java
+++ src/main/java/models/NonEndangeredAnimal.java
@@ -1,5 +1,9 @@
 package models;
 
+import org.sql2o.Connection;
+
+import java.util.List;
+
 public class NonEndangeredAnimal extends Animal implements DatabaseManagement {
 
     public static final String DATABASE_TYPE = "NonEndangeredAnimal";
@@ -9,5 +13,25 @@ public class NonEndangeredAnimal extends Animal implements DatabaseManagement {
         this.animalId = animalId;
         this.age = age;
         type = DATABASE_TYPE;
+    }
+
+    public static List<NonEndangeredAnimal> all() {
+        String sql = "SELECT * FROM animals WHERE type='NonEndangeredAnimal';";
+        try(Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(NonEndangeredAnimal.class);
+        }
+    }
+
+    public static NonEndangeredAnimal find(int id) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM animals where id=:id";
+            NonEndangeredAnimal animal = con.createQuery(sql)
+                    .addParameter("id", id)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetchFirst(NonEndangeredAnimal.class);
+            return animal;
+        }
     }
 }
--- src/test/java/models/NonEndangeredAnimalTest.java
+++ src/test/java/models/NonEndangeredAnimalTest.java
@@ -1,9 +1,6 @@
 package models;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 import static org.junit.Assert.*;
 
@@ -16,6 +13,9 @@ public class NonEndangeredAnimalTest {
     @After
     public void tearDown() throws Exception {
     }
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
 
     @Test
     public void nonEndangeredAnimal_instantiatesCorrectlyWithObjects_true() {
@@ -36,7 +36,49 @@ public class NonEndangeredAnimalTest {
 
     @Test
     public void NonEndangeredAnimal_instantiatesWithAnimalId_int() {
-        NonEndangeredAnimal testNonEndangeredAnimal = new NonEndangeredAnimal("Eagle", 1, 3);
+        NonEndangeredAnimal testNonEndangeredAnimal = new NonEndangeredAnimal("Mongoose", 1, 3);
         assertEquals(1, testNonEndangeredAnimal.getAnimalId());
     }
+    @Test
+    public void equals_returnsTrueIfNameAndPersonIdAreSame_true() {
+        NonEndangeredAnimal testNonEndangeredAnimal = new NonEndangeredAnimal("Mongoose", 1, 3);
+        NonEndangeredAnimal anotherNonEndangeredAnimal = new NonEndangeredAnimal("Mongoose", 1, 3);
+        assertTrue(testNonEndangeredAnimal.equals(anotherNonEndangeredAnimal));
+    }
+
+    @Test
+    public void save_successfullyAddsNonEndangeredAnimalToDatabase_List() {
+        NonEndangeredAnimal testNonEndangeredAnimal = new NonEndangeredAnimal("Mongoose", 1, 3);
+        testNonEndangeredAnimal.save();
+        assertTrue(NonEndangeredAnimal.all().get(0).equals(testNonEndangeredAnimal));
+    }
+
+    @Test
+    public void save_assignsIdToNonEndangeredAnimal() {
+        NonEndangeredAnimal testNonEndangeredAnimal = new NonEndangeredAnimal("Mongoose", 1, 3);
+        testNonEndangeredAnimal.save();
+        NonEndangeredAnimal savedNonEndangeredAnimal = NonEndangeredAnimal.all().get(0);
+        assertEquals(savedNonEndangeredAnimal.getId(), testNonEndangeredAnimal.getId());
+    }
+
+    @Test
+    public void all_returnsAllInstancesOfNonEndangeredAnimal_true() {
+        NonEndangeredAnimal firstNonEndangeredAnimal = new NonEndangeredAnimal("Mongoose", 1, 3);
+        firstNonEndangeredAnimal.save();
+        NonEndangeredAnimal secondNonEndangeredAnimal = new NonEndangeredAnimal("Plain Zebra", 3, 5);
+        secondNonEndangeredAnimal.save();
+        assertEquals(true, NonEndangeredAnimal.all().get(0).equals(firstNonEndangeredAnimal));
+        assertEquals(true, NonEndangeredAnimal.all().get(1).equals(secondNonEndangeredAnimal));
+    }
+
+    @Test
+    public void find_returnsNonEndangeredAnimalWithSameId_secondNonEndangeredAnimal() {
+        NonEndangeredAnimal firstNonEndangeredAnimal = new NonEndangeredAnimal("Mongoose", 1, 3);
+        firstNonEndangeredAnimal.save();
+        NonEndangeredAnimal secondNonEndangeredAnimal = new NonEndangeredAnimal("Plain Zebra", 1, 4);
+        secondNonEndangeredAnimal.save();
+        assertEquals(NonEndangeredAnimal.find(secondNonEndangeredAnimal.getId()), secondNonEndangeredAnimal);
+    }
+
+
 }
