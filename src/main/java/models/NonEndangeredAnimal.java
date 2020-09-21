37db4626d910e62f5750fa58740ee49c72ace6e6
George Okumu <gokumu12@gmail.com>
2020-09-21 08:10:40 +0300

    Done tests to see if nonEndangered animal class instantiates with animalName, animalId and animalAge correctly.

--- src/main/java/models/NonEndangeredAnimal.java
+++ src/main/java/models/NonEndangeredAnimal.java
@@ -2,4 +2,12 @@ package models;
 
 public class NonEndangeredAnimal extends Animal implements DatabaseManagement {
 
+    public static final String DATABASE_TYPE = "NonEndangeredAnimal";
+
+    public NonEndangeredAnimal(String animalName, int animalId, int age) {
+        this.animalName = animalName;
+        this.animalId = animalId;
+        this.age = age;
+        type = DATABASE_TYPE;
+    }
 }
--- src/test/java/models/NonEndangeredAnimalTest.java
+++ src/test/java/models/NonEndangeredAnimalTest.java
@@ -1,7 +1,9 @@
 package models;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
@@ -15,5 +17,26 @@ public class NonEndangeredAnimalTest {
     public void tearDown() throws Exception {
     }
 
+    @Test
+    public void nonEndangeredAnimal_instantiatesCorrectlyWithObjects_true() {
+        NonEndangeredAnimal testNonEndangeredAnimal = new NonEndangeredAnimal("mongoose",1, 3);
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void nonEndangeredAnimal_getsAnimalNameCorrectly_true() {
+        NonEndangeredAnimal testNonEndangeredAnimal = new NonEndangeredAnimal("Mongoose", 1, 3);
+        assertEquals("Mongoose",testNonEndangeredAnimal.getAnimalName());
+    }
 
+    @Test
+    public void nonEndangeredAnimal_getsAnimalAgeCorrectly_true() {
+        NonEndangeredAnimal testNonEndangeredAnimal = new NonEndangeredAnimal("Mongoose", 1, 3);
+        assertEquals(3, testNonEndangeredAnimal.getAge());
+    }
+
+    @Test
+    public void NonEndangeredAnimal_instantiatesWithAnimalId_int() {
+        NonEndangeredAnimal testNonEndangeredAnimal = new NonEndangeredAnimal("Eagle", 1, 3);
+        assertEquals(1, testNonEndangeredAnimal.getAnimalId());
+    }
 }
